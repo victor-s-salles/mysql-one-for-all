79923883efa7a681e6cce2cd635b1dc2bfebbf1d
Victor Salles <victor.salles@engenharia.ufjf.br>
2023-01-26 17:25:14 -0300

    Adicionado data da assinatura do usuario

--- desafio1.sql
+++ desafio1.sql
@@ -39,6 +39,7 @@ CREATE TABLE
         nome VARCHAR(200) NOT NULL,
         idade INT NOT NULL,
         plano_id INT NOT NULL,
+        data_assinatura DATE,
         FOREIGN KEY (plano_id) REFERENCES plano (plano_id)
     ) engine = InnoDB;
 
@@ -46,6 +47,7 @@ CREATE TABLE
     SpotifyClone.historico(
         usuario_id INT NOT NULL,
         cancao_id INT NOT NULL,
+        data_reproducao DATE,
         CONSTRAINT PRIMARY KEY (usuario_id, cancao_id),
         FOREIGN KEY (usuario_id) REFERENCES usuario (usuario_id),
         FOREIGN KEY (cancao_id) REFERENCES cancao (cancao_id)
@@ -93,5 +95,60 @@ VALUES ('BREAK MY SOUL', 1, 279), ('VIRGO’S GROOVE', 1, 369), ('ALIEN SUPERSTA
     ), ('Samba em Paris', 6, 267), ('The Bard’s Song', 7, 244), ('Feeling Good', 8, 100);
 
 INSERT INTO
-    SpotifyClone.usuario (nome, idade, plano_id)
-VALUES ('Barbara Liskov', 82, 1), ('Robert Cecil Martin', 58, 1), ('Ada Lovelace', 37, 4), ('Martin Fowler', 46, 4), ('Sandi Metz', 58, 4), ('Paulo Freire', 19, 2), ('Bell Hooks', 26, 2), ('Christopher Alexander', 85, 3), ('Judith Butler', 45, 3), ('Jorge Amado', 58, 3);
+    SpotifyClone.usuario (
+        nome,
+        idade,
+        plano_id,
+        data_assinatura
+    )
+VALUES (
+        'Barbara Liskov',
+        82,
+        1,
+        '2019-10-20'
+    ), (
+        'Robert Cecil Martin',
+        58,
+        1,
+        '2017-01-06'
+    ), (
+        'Ada Lovelace',
+        37,
+        4,
+        '2017-12-30'
+    ), (
+        'Martin Fowler',
+        46,
+        4,
+        '2017-01-17'
+    ), (
+        'Sandi Metz',
+        58,
+        4,
+        '2018-04-29'
+    ), (
+        'Paulo Freire',
+        19,
+        2,
+        '2018-02-14'
+    ), (
+        'Bell Hooks',
+        26,
+        2,
+        '2018-01-05'
+    ), (
+        'Christopher Alexander',
+        85,
+        3,
+        '2019-06-05'
+    ), (
+        'Judith Butler',
+        45,
+        3,
+        '2020-05-13'
+    ), (
+        'Jorge Amado',
+        58,
+        3,
+        '2017-02-17'
+    );
